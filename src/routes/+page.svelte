21b2896ae626beb07e0eec263d95dcc13b586309
Gabriel Egli <mail@gabriel-egli.ch>
2023-08-05 00:51:21 +0200

    setup done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="testpage">Test Page</h1>
+
+<style>
+	.testpage {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+		font-size: 75px;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		.testpage {
+			background: #2d2d2d;
+			color: aliceblue;
+			outline: 5px dashed #000;
+		}
+	}
+</style>
